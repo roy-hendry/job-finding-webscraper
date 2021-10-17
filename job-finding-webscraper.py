d56cf2911163aa327d7112ade14efb6768d83bdf
roy-hendry <royjairusraj@hotmail.com>
2021-10-17 14:28:40 +0100

    v2 Finding first 15 job names from Indeed obtained

--- job-finding-webscraper.py
+++ job-finding-webscraper.py
@@ -17,20 +17,43 @@ time.sleep(3)
 clear = lambda: os.system("cls")
 clear()
 
-searchTerm = input("What job would you like to search for?")
+#searchTerm = input("What job would you like to search for?")
+searchTerm = "graduate software developer"
+#searchTerm = "test"
 
 driver.get("https://uk.indeed.com/?from=gnav-jobsearch--jasx")
+#driver.get("https://techwithtim.net")
 
 search = driver.find_element_by_id("text-input-what")
+#search = driver.find_element_by_id("s")
 search.send_keys(searchTerm)
 search.send_keys(Keys.RETURN)
 
-"""""
+
 try:
-    main = WebDriver(driver, 10).until(
+    jobCards = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "mosaic-provider-jobcards"))
+    )
+    titles = jobCards.find_elements_by_tag_name("span")
+    for title in titles:
+        #header = title.find_element_by_xpath("//div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[1]/h2/span").get_attribute("title")
+        header = title.get_attribute("title")
+        if not len(header.strip()) == 0:
+            print(header)
+
+finally:
+    driver.quit()
+
+"""
+try:
+    main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "main"))
     )
-    print(main.text)
-except:
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-header").find_element_by_class_name("entry-meta").find_element_by_class_name("posted-on")
+        print(header.text)
+
+finally:
     driver.quit()
 """
