a0ae89d2a90d40f26e0efc47e59f391ccee2bd6f
roy-hendry <royjairusraj@hotmail.com>
2021-10-18 21:10:08 +0100

    v4 Fully working on Indeed

--- job-finding-webscraper.py
+++ job-finding-webscraper.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import time
 
+#Finds the path to my Chromedriver, we will interact with this a lot
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
@@ -17,69 +18,59 @@ time.sleep(3)
 clear = lambda: os.system("cls")
 clear()
 
-#searchTerm = input("What job would you like to search for?")
-searchTerm = "graduate software developer"
-#searchTerm = "test"
+#Allows the user to input what type of job they are searching for
+searchTerm = input("What job would you like to search for? ")
+#searchTerm = "graduate software developer"
 
+#Go to this website
 driver.get("https://uk.indeed.com/?from=gnav-jobsearch--jasx")
-#driver.get("https://techwithtim.net")
 
+#Find the text box and enter the type of job the user entered earlier
 search = driver.find_element_by_id("text-input-what")
-#search = driver.find_element_by_id("s")
 search.send_keys(searchTerm)
 search.send_keys(Keys.RETURN)
 
+#Create empty arrays of each of the types of data we want to collect about the job so we can append to them later
 jobTitles = []
 jobEmployers = []
 jobLinks = []
 
+#Try to locate the id mentioned below, if it is not found within 10 seconds then close the program
 try:
     jobCards = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "mosaic-provider-jobcards"))
     )
-    titles = jobCards.find_elements_by_tag_name("span")
-    for title in titles:
-        val = title.get_attribute("title")
+
+    #Collects all of the job titles on the page
+    elements = jobCards.find_elements_by_tag_name("span")
+    for x in elements:
+        val = x.get_attribute("title")
+        #Make sure that the empty values aren't shown
         if not len(val.strip()) == 0:
             jobTitles.append(val)
-            #print(val)
 
-#Fix this, it doesn't pick up the company names (it works if they are clickable)
-    titles = jobCards.find_elements_by_tag_name("pre")
-    for title in titles:
-        val = title.get_attribute("companyName")
+    #Collects all of the employers for the jobs
+    elements = jobCards.find_elements_by_class_name("companyName")
+    for x in elements:
+        val = x.text
         jobEmployers.append(val)
-        print(val)
 
-    titles = jobCards.find_elements_by_class_name("tapItem")
-    for title in titles:
-        val = title.get_attribute("href")
+    #Collects all of the job links
+    elements = jobCards.find_elements_by_class_name("tapItem")
+    for x in elements:
+        val = x.get_attribute("href")
         jobLinks.append(val)
 
+    #Clears the terminal
+    clear()
+
+    #Prints all of the job titles, employers and links respectively
     for i in range(len(jobTitles)):
         print("")
         print("jobTitles: " + jobTitles[i])
+        print("jobEmployers: " + jobEmployers[i])
         print("jobLinks: " + jobLinks[i])
 
-    #for i in range(len(jobEmployers)):
-        #print("")
-        #print("jobEmployers: " + jobEmployers[i])
-        #print(i)
-
-finally:
-    print("")
-    #driver.quit()
-
-"""
-try:
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
-    )
-    articles = main.find_elements_by_tag_name("article")
-    for article in articles:
-        header = article.find_element_by_class_name("entry-header").find_element_by_class_name("entry-meta").find_element_by_class_name("posted-on")
-        print(header.text)
-
+#After everything has been done close the program
 finally:
     driver.quit()
-"""
